404f224ab21dc32f4f0d5940c19abd19b0e773f4
alexalok <alexalok@yandex.ru>
2018-04-08 22:34:28 +0300

    Added MouseSimulatorExtended.

---
+++ MouseSimulatorExtended/IMouseSimulatorExtensions.cs
@@ -0,0 +1,16 @@
+﻿using WindowsInput;
+
+namespace MouseSimulatorExtended
+{
+    public static class MouseSimulatorExtensions
+    {
+        public static void MoveToByResolution(this IMouseSimulator mouse, int x, int y)
+        {
+            double screenWidth = System.Windows.SystemParameters.PrimaryScreenWidth;
+            double screenHeight = System.Windows.SystemParameters.PrimaryScreenHeight;
+            double calcX = 65535 / screenWidth * x;
+            double calcY = 65535 / screenHeight * y;
+            mouse.MoveMouseTo(calcX, calcY);
+        }
+    }
+}
