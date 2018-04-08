677aa3aa114b1617d03c16a6eefecdb73ed684a6
alexalok <alexalok@yandex.ru>
2018-04-08 22:35:33 +0300

    Updated aDevLibStandard to multi-target netcore20 and net45.

--- aDevLibStandard/Extensions/ProcessExtensions.cs
+++ aDevLibStandard/Extensions/ProcessExtensions.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using JetBrains.Annotations;
 
 namespace aDevLibStandard.Extensions
 {
