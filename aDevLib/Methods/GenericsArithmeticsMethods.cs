8a98d67fdf683ce43ef501c004ddf691cd0ddfb5
alexalok <alexalok@yandex.ru>
2018-04-17 15:42:50 +0300

    Added GenericsArithmeticsMethods

---
+++ aDevLib/Methods/GenericsArithmeticsMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace aDevLib.Methods
+{
+    public static class GenericsArithmeticsMethods<T>
+    {
+        public static T Add(params T[] members)
+        {
+            var membersStack = new Stack<T>(members);
+            while (membersStack.Count > 1)
+            {
+                dynamic a = membersStack.Pop();
+                dynamic b = membersStack.Pop();
+                var sum = a + b;
+                membersStack.Push(sum);
+            }
+            return membersStack.Pop();
+        }
+    }
+}
