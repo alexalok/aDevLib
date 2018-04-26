8073a46c164dbb5e613b3c0f5e29d44f98c5401e
alexalok <alexalok@yandex.ru>
2018-04-27 00:49:38 +0300

    Added StringExtensions with SubstringFromEnd method.

---
+++ aDevLib/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace aDevLib.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string SmartSubstring(this string @string, int startIndex, int length)
+        {
+            return length >= 0 ?
+                @string.Substring(startIndex, length) :
+                @string.Substring(startIndex, @string.Length + length - startIndex);
+        }
+    }
+}
